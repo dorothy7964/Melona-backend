2bc62369f8d294d0c048320143b79a6a0437ffaf
sample <dorothy_007@naver.com>
2020-03-09 22:10:44 +0900

    #2.3 middlewares

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares"; 
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,9 +12,8 @@ console.log(process.env.PORT);
 
 const server = new GraphQLServer({
     schema,
-    context: ({ request }) => ({ request })
+    context: ({ request }) => ({ request, isAuthenticated })
 });
-
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
