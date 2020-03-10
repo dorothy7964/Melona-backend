c2fd4c8f173ce884ec9774172565b767ce3822d2
sample <dorothy_007@naver.com>
2020-03-10 20:35:40 +0900

    #2.12 User - oneUser

---
+++ src/api/User/oneUser/oneUser.js
@@ -0,0 +1,16 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        oneUser: async(_, args) => {
+            const { userName } = args;
+            const exists = await prisma.$exists.user({userName});
+                
+            if (exists) {
+                return prisma.user({ userName });
+            } else {
+                throw Error("User not found");
+            }
+        }
+    }
+};
