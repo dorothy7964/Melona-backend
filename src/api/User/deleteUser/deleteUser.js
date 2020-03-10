ed15cf858c8c1243b20f755f1f3fda9776b2fcbc
sample <dorothy_007@naver.com>
2020-03-10 20:46:26 +0900

    #2.15 User - deleteUser

---
+++ src/api/User/deleteUser/deleteUser.js
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        deleteUser: async(_, __, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            try {
+                await prisma.deleteUser({
+                    id: user.id
+                });
+                return true;
+            } catch(e) {
+                console.log(e);
+                return false;
+            }
+            
+        } 
+    }
+};
