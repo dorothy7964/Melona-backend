34278eac6079f81566c9ac5b907600e32309ad7d
sample <dorothy_007@naver.com>
2020-04-24 06:59:25 +0900

    #7.3 Profile - searchUser

--- src/api/User/searchUser/searchUser.js
+++ src/api/User/searchUser/searchUser.js
@@ -2,18 +2,25 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
     Query: {
-        searchUser: async(_, args) => {
+        searchUser: async(_, args, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
             const { term } = args;
 
             if (term.length > 0) {
-                const users = await prisma.users({
+                const containUser = await prisma.users({
                     where: {
                         OR: [
-                            { userName_contains: args.term },
-                            { email_contains: args.term }
+                            { userName_contains: term },
+                            { email_contains: term }
                         ]
                     }
                 });
+
+                const users = containUser.filter(containUser => (
+                    containUser.userName !== user.userName
+                ));
+
                 return users;
             } else {
                 throw Error("Please enter a character to Serch");
