09d78848403fb9325b4febdc97cf9accdb934f9b
sample <dorothy_007@naver.com>
2020-05-22 00:44:27 +0900

    # seeBuyMeGroup Pagination

--- src/api/Post/seeBuyMeGroup/seeBuyMeGroup.js
+++ src/api/Post/seeBuyMeGroup/seeBuyMeGroup.js
@@ -3,9 +3,11 @@ import { prisma } from '../../../../generated/prisma-client';
 export default {
     Query: {
         seeBuyMeGroup: (_, args) => {
-            const { groupRoomId } = args;
+            const { groupRoomId, items, pageNumber } = args;
 
             return prisma.posts({
+                first: items,
+                skip: pageNumber,
                 where: {
                     anotherPage: true,
                     groupRoom: groupRoomId
