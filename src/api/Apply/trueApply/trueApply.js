016dd2489c0fa2f29fb69ef346eddccd24b02bf6
sample <dorothy_007@naver.com>
2020-11-01 17:01:11 +0900

    # Eliminate the Unnecessary

--- src/api/Apply/trueApply/trueApply.js
+++ src/api/Apply/trueApply/trueApply.js
@@ -7,13 +7,6 @@ export default {
             const { user } = request;
             const { postId } = args;
 
-            const filterOptions = {
-                AND: [
-                    { user: {id: user.id} },
-                    { post: {id: postId} }
-                ]
-            };
-
             try {
                 const existingApply = await prisma.$exists.apply({
                     AND: [
